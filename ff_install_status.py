2db5afcf8481f8c12e52f4e1d3436a7b2749b723
nempd <809745650@qq.com>
2021-08-19 00:21:00 +0800

    ff_install_status

--- ff_install_status.py
+++ ff_install_status.py
@@ -1,9 +1,21 @@
-#dev/rct/prlive
-#安卓普通
-#安卓max
-#第三方版本监控:huawei/sumsung/3rd party
-#ios版本普通
-#ios版本max
+# dev/rct/prlive
+# 安卓普通
+# 安卓max
+# 第三方版本:huawei/sumsung/3rd party
+# ios版本普通
+# ios版本max
+# 定时监测
+# log：时间，状态
 
-#定时监测
-#log：时间，状态
+import os
+import time
+
+# a = os.path.exists('/Users/lu/Desktop/export.json') #判断指定文件是否存在
+# print(a)
+
+walk(top, topdown=True, οnerrοr=None, followlinks=False) #遍历文件夹名称
+
+# Dict_Installation_package = { #安装包目录，标明更新时间
+#     "文件名":"2021-8-18"
+# }
+
