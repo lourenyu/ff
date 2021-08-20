900175047e5dfcffc6b92f415206df8850589590
Nempd <809745650@qq.com>
2021-08-20 18:38:16 +0800

    delete

--- main_smoke.py
+++ main_smoke.py
@@ -1,19 +1,22 @@
-#登陆
-
 import uiautomator2 as u2
+import time
 
 order = u2.connect('RFCNA0FJPXY') #链接手机&模拟器
 #安装包
 order(text="Free Fire").click() #打开应用Free Fire
 # order(text="Free Fire MAX").click() #打开应用Free Fire MAX
+
 order.click(0.685, 0.651)#判断--权限说明
+
 order.click(0.488, 0.671)#判断--照片访问d.click(0.488, 0.671)
 
 #garena logo
 #loding
 #登陆页面
 order.click(0.445, 0.889) #游客登陆
+
 order.click(0.497, 0.848) #TAP TO BEGIN
+
 #弹窗-WHATS NEW
 #弹窗-活动-回归老兵
 #弹窗-活动-日常任务
