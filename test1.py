9bb3cc9ab37045bce5f5a7aed0b65eede7a64f58
nempd <809745650@qq.com>
2021-08-21 23:55:29 +0800

    testcode

--- test1.py
+++ test1.py
@@ -1,27 +1,10 @@
-import pyautogui as pg
-import uiautomator2 as u2
-import numpy as np
 import cv2
-
-# screenWidth, screenHeight = pg.size() #获取当前屏幕分辨率
-# print(screenWidth, screenHeight)
-# pg.moveTo(screenWidth / 2, screenHeight / 2,3) #移动鼠标到屏幕中间
-# pg.FAILSAFE = False #鼠标位置边界警告=关闭
-# currentMouseX, currentMouseY = pg.position() #获取当前鼠标位置
-# print(currentMouseX,currentMouseY)
-# pg.moveTo(currentMouseX,currentMouseY,0) #移动鼠标到指定位置
-# pg.PAUSE = 0.5 #全局pg函数延迟
-# pg.click(x=currentMouseX, y=currentMouseY, clicks=2, interval=0.0, button= "left", duration=0.0, tween=pg.linear)
-# im2 = pg.screenshot(r'.\Source_Pics\Result_Pics\首次截屏.png')
-
-# img1 = cv2.imread('ceshi.jpg') #读取图像
-# cv2.imshow('原始图像', img1)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+img = cv2.imread(r".\Source_Pics\Test_Pics\test_case.png",cv2.IMREAD_GRAYSCALE)
+img1 = cv2.imread(r".\Source_Pics\Test_Pics\test_case.png",cv2.IMREAD_COLOR)
 
 
-img = cv2.imread("test_case.png")  #导入图片
-cv2.namedWindow("Image")  #命名容器
-cv2.imshow("Image",img)  #窗口展示
-cv2.waitKey(0)   #刷新率
-cv2.destroyAllWindows()  #close
+print(img.shape)
+print(img.size)
+print(img.dtype)
+
+
--- test_opencv.py
+++
@@ -1,7 +0,0 @@
-import cv2
-
-img = cv2.imread("test1.jpg")  #导入图片
-cv2.namedWindow("Image")  #命名容器
-cv2.imshow("Image",img)
-cv2.waitKey(0)   #刷新率
-cv2.destroyAllWindows()  #close
