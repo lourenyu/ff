b18813d0c51bcab4da030947643e7edb8eb41462
Nempd <809745650@qq.com>
2021-08-23 19:04:44 +0800

    plus

--- test_try.py
+++ test_try.py
@@ -34,14 +34,43 @@
 # cv.waitKey(0)
 # cv.destroyAllWindows()
 
+# import cv2 as cv
+# img1 = cv.imread(r"test_case.png")
+# img = img1[0:426,43:400]
+# img2 = cv.imread(r"test_case2.png")
+# img3 = img2[0:426,43:400]
+# blend = cv.addWeighted(img,0.5,img3,0.5,0)  #两张图的大小和通道也应该相同
+# cv.imshow("blend",blend)
+# cv.waitKey()
+# cv.destroyAllWindows()
+
+
 import cv2 as cv
+import matplotlib.pyplot as plt
+
 img1 = cv.imread(r"test_case.png")
-img = img1[0:426,43:400]
+# img = img1[0:426,43:400]
+rows,cols = img1.shape[0:2]
 img2 = cv.imread(r"test_case2.png")
-img3 = img2[0:426,43:400]
+# img = img1[0:426,43:400]
+roi = img2[0:rows,0:cols]
+img1_gray = cv.cvtColor(img1,cv.COLOR_BGR2GRAY)
 
-# blend = cv.addWeighted(img,0.5,img3,0.9,0)  #两张图的大小和通道也应该相同
+ret,img1_thres = cv.threshold(img1_gray,200,255,cv.THRESH_BINARY_INV)
+img1_fg =cv.add(img1,img1,mask=img1_thres)    #拿到logo图案的前景
 
-cv.imshow("blend",img)
-cv.waitKey()
+img1_thres_inv = cv.bitwise_not(img1_thres)
+roi_bg = cv.add(roi,roi,mask=img1_thres_inv)  #拿到roi图案的背景
+
+img_add = cv.add(img1_fg,roi_bg)     #背景和前景相加
+img2[0:rows,0:cols] = img_add
+
+cv.imshow("gray",img1_gray)
+cv.imshow("thres",img1_thres)
+cv.imshow("fg",img1_fg)
+cv.imshow("tinv",img1_thres_inv)
+cv.imshow("roi_bg",roi_bg)
+cv.imshow("img_add",img_add)
+cv.imshow("img2",img2)
+cv.waitKey(0)
 cv.destroyAllWindows()
