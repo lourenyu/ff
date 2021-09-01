d8f30af27796a85b98630230013e8276ac60d909
nempd <809745650@qq.com>
2021-09-01 23:38:52 +0800

    update

--- test_pyautogui.py
+++ test_pyautogui.py
@@ -1,13 +1,10 @@
 import cv2
-img = cv2.imread(r".\Source_Pics\Test_Pics\test_case.png",cv2.IMREAD_GRAYSCALE)
-img1 = cv2.imread(r".\Source_Pics\Test_Pics\test_case.png",cv2.IMREAD_COLOR)
-
-# img1 = cv2.imread('ceshi.jpg') #读取图像
-# cv2.imshow('原始图像', img1)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
-print(img.shape)
-print(img.size)
-print(img.dtype)
+img = cv2.imread(r"./Source_Pics/Test_Pics/test_case.png")
+cv2.imshow('orgin1', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+print(img.shape) #读（高height，宽length，通道数）
+print(img.size) #读取大小 height*length*通道数
+print(img.dtype) #图片类型
 
 
